7981884663d090fbf04481943b34de084cf30321
Chandrasekar_V <ninjuchandra@gmail.com>
2021-06-04 23:38:30 +0530

    files and commands for setting up cricket database

--- sql_files/config_script.sql
+++ sql_files/config_script.sql
@@ -1,21 +1,12 @@
--- from pdb sys
+-- Connect to PDB using SYS user and grant permissions to pdbadmin dba user
 grant unlimited tablespace to pdbadmin;
 grant resource, connect, dba to pdbadmin;
 commit;
 
--- from pdbadmin
-create table pdb_19c (message varchar2(200));
-insert into pdb_19c values ('Welcome to Pluggable DB');
-commit;
-select * from pdb_19c; 
-
-SELECT banner FROM v$version WHERE ROWNUM = 1;
-select * from dba_user_role where grantee = 'cric_batch_user';
-
+-- Connect to PDB from pdbadmin 
 CREATE USER cric_batch_user IDENTIFIED BY "cric_b@tch_pwd";
 grant create session to cric_batch_user;
 alter user cric_batch_user quota unlimited on users;
-grant create any table to cric_batch_user;
 
 CREATE USER test_schema no authentication;
 alter user test_schema quota unlimited on users;
@@ -23,6 +14,7 @@ alter user test_schema quota unlimited on users;
 create table test_schema.test_table (s_no number, p_name varchar2(50));
 grant select, insert, update, delete on test_schema.test_table to cric_batch_user;
 
+-- Connect to PDB from cric_batch_user
 insert into test_schema.test_table select 1,'jayashree' from dual;
 insert into test_schema.test_table select 2,'chandrasekar' from dual;
 commit;
